e8b321da5dea4c45dc0ed465708dcfb62086841f
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2024-04-13 16:04:18 +0200

    Finished course

---
+++ 17_delegationOrientedDesign.js
@@ -0,0 +1,5 @@
+/** What is delegation? (chatgpt)
+ * In programming, delegation refers to a design pattern where an object passes on a task to another object to perform the actual work. Instead of performing the task itself, the delegating object keeps a reference to the delegate object and invokes its methods to accomplish the task.
+ * Delegation promotes code reuse and modularity by allowing objects to collaborate without tightly coupling them together. It's often used to implement composition over inheritance, where classes can be composed of smaller, more focused components rather than inheriting behavior from a superclass.
+ * It's actually waht JavaScript does, delegation not inheritance (copying)
+ */
